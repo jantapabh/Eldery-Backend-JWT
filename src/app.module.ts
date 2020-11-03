372defe93b0bbfee1fdaad0673b76873a8c52276
FrameTee29 <teeraphat123@hotmail.com>
2020-11-03 20:33:03 +0700

    create product

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountModule } from './account/account.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [AccountModule],
+  imports: [AccountModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
