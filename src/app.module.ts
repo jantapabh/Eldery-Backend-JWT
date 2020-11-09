ab33af1ff7c2402a6452b0cf0b0f7de8631edd01
FrameTee29 <teeraphat123@hotmail.com>
2020-11-09 17:55:54 +0700

    npm passport & nest auth & nest user

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { AccountModule } from './account/account.module';
 import { ProductModule } from './product/product.module';
 import { UploadimageModule } from './uploadimage/uploadimage.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AccountModule, ProductModule, UploadimageModule],
+  imports: [AccountModule, ProductModule, UploadimageModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
