e528b580c5bb49e3790c45fc28384c0a18e0e594
FrameTee29 <teeraphat123@hotmail.com>
2020-11-09 18:23:29 +0700

    acess_token

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,15 +2,16 @@ import { Controller, Get , Request, Post, UseGuards} from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthGuard } from '@nestjs/passport';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,private authService:AuthService) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user);
   }
   
 
