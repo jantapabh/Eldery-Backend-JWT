3198dba3f0416773de4b61de8e091e8118f69bfc
FrameTee29 <teeraphat123@hotmail.com>
2020-11-11 21:55:08 +0700

    can upload images

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountModule } from './account/account.module';
 import { ProductModule } from './product/product.module';
-
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { FilesModule } from './files/files.module';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
-  imports: [AccountModule, ProductModule, AuthModule, UsersModule],
+  imports: [
+    AccountModule,
+    ProductModule,
+    AuthModule,
+    UsersModule,
+    FilesModule,
+    MulterModule.register({
+      dest: './uploads',
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
