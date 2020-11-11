17461f737f9ca9e41b45fdc891bdd31342f39b33
FrameTee29 <teeraphat123@hotmail.com>
2020-11-11 21:30:25 +0700

    start product

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -100,22 +100,4 @@ export class AuthService {
 
 
 
-  async validateUser(username: string, pass: string): Promise<any> {
-    // const user = await this.usersService.findOne(username);
-    // if (user && user.password === pass) {
-    //   const { password, ...result } = user;
-    //   return result;
-    // }
-    // return null;
-  }
-
-  // async login(user: any) {
-  //   const payload = { username: user.username, sub: user.userId };
-  //   return {
-  //     access_token: this.jwtService.sign(payload),
-  //   };
-  // }
-
-
-
 }
--- src/product/entitys/product.entity.ts
+++ src/product/entitys/product.entity.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 import {  DOUBLE } from 'sequelize';
-import { Table, Column, Model, AutoIncrement, PrimaryKey } from 'sequelize-typescript';
+import { Table, Column, Model, AutoIncrement, PrimaryKey, DataType, AllowNull } from 'sequelize-typescript';
 
 @Table
 export class Products extends Model<Products> {
@@ -10,24 +10,28 @@ export class Products extends Model<Products> {
   @Column
   p_id:number;
 
-  @Column  
+  @Column(DataType.STRING(2500)) 
   name: string;
 
   @Column({type:DOUBLE })
   price: number;
 
+  @AllowNull(true)
   @Column
   size: string;
 
+  @AllowNull(true)
   @Column
   qtystock: number;
 
   @Column
   category: string;
 
+  @AllowNull(true)
   @Column
   brand: string;
 
+  @AllowNull(true)
   @Column
   detail: string;
 
